feffa29a673854605a8f860915c73d95e8a5347c
chenmoonmo <chenmoonmo@gmail.com>
2022-12-13 15:57:04 +0800

    v1.0.4

--- src/view/Editor/index.tsx
+++ src/view/Editor/index.tsx
@@ -2,6 +2,7 @@ import { ComponentType } from 'react';
 import EditorProvider from 'view/EditorProvider';
 import EditorStage from 'view/EditorStage';
 import HistoryProvider from 'view/HistoryProvider';
+import { Global, css } from '@emotion/react';
 
 export type EditorPropsType = {
   image: string;
@@ -15,6 +16,13 @@ const Editor: ComponentType<EditorPropsType> = ({ image, width, height }) => {
   return (
     <EditorProvider width={width} height={height}>
       <HistoryProvider image={image}>
+        <Global
+          styles={css`
+            * {
+              box-sizing: border-box;
+            }
+          `}
+        />
         <EditorStage />
       </HistoryProvider>
     </EditorProvider>
