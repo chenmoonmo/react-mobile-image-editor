90d5f6080c362f664f10858f719e9d36dfca426e
chenmoonmo <chenmoonmo@gmail.com>
2022-12-20 18:48:21 +0800

    feat: config rollup dev env

---
+++ rollup.config.dev.ts
@@ -0,0 +1,42 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import postcss from 'rollup-plugin-postcss';
+import replace from '@rollup/plugin-replace';
+import babel from '@rollup/plugin-babel';
+import path from 'path';
+import svgr from '@svgr/rollup';
+
+export default {
+  input: `example/src/index.tsx`,
+  output: {
+    file: 'example/assets/bundle.js',
+    format: 'iife',
+    sourcemap: true,
+    globals: {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+    },
+  },
+  watch: {
+    include: ['src/**', 'example/**'],
+    exclude: ['example/assets/**'],
+  },
+  plugins: [
+    resolve({
+      browser: true,
+    }),
+    svgr({ icon: true }),
+    commonjs({
+      include: /node_modules/,
+    }),
+    babel(),
+    typescript({
+      useTsconfigDeclarationDir: true,
+    }),
+    postcss({
+      extract: path.resolve('example/assets/bundle.css'),
+    }),
+    replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
+  ],
+};
